bf9fc24e824802e2f1a3e1c5bb0abf18c8f36f18
NoobZik <noobzik@pm.me>
2025-04-07 00:01:23 +0200

    Reverted to SparkContext

--- src/main/scala/wikipedia/WikipediaRankingInterface.scala
+++ src/main/scala/wikipedia/WikipediaRankingInterface.scala
@@ -1,5 +1,7 @@
 package wikipedia
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.SparkSession._
